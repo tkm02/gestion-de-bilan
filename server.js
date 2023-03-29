35c0e9eb0a4ccb0b59f2031ae801f29556d2e889
tkm02 <mamadoutuo77@gmail.com>
2023-03-29 08:06:24 +0000

    env-2

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const http = require('http');
 const app = require('./app');
-const port = 5010;
+// const port = 5010;
 const server = http.createServer(app);
 
 server.on('listening', () => {
   console.log('Listening on ' + port);
 });
 
-server.listen(port);
+server.listen(5010);
